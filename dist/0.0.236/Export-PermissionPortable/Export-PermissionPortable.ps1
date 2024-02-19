766591af9296ba9c9f3a730fda270c03440efa41
Jeremy La Camera <IMJLA@users.noreply.github.com>
2024-02-18 16:37:51 -0800

    its alive

--- dist/0.0.236/Export-PermissionPortable/Export-PermissionPortable.ps1
+++ dist/0.0.236/Export-PermissionPortable/Export-PermissionPortable.ps1
@@ -1,6 +1,6 @@
 <#PSScriptInfo
 
-.VERSION 0.0.235
+.VERSION 0.0.236
 
 .GUID c7308309-badf-44ea-8717-28e5f5beffd5
 
@@ -30,6 +30,7 @@ its alive
 .PRIVATEDATA
 
 #> 
+
 
 
 
--- docs/en-US/Export-Permission.ps1.md
+++ docs/en-US/Export-Permission.ps1.md
@@ -1,6 +1,6 @@
 ---
 external help file: -help.xml
-help version: 0.0.234
+help version: 0.0.235
 locale: en-US
 script name: Export-Permission.ps1
 online version:
